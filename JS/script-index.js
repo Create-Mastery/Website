b1cbe82c8cfad673832ff1df39bbda76f3b088e1
Sckab <giulianodeamicis2009@gmail.com>
2025-03-17 17:08:58 +0100

    Modifica e ordinamento cartella

---
+++ JS/script-index.js
@@ -0,0 +1,25 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+const navbar = document.getElementById('navbar');
+
+window.onscroll = function () {
+    if (window.scrollY > 200) {
+        navbar.classList.add('fixed');
+    } else {
+        navbar.classList.remove('fixed');
+    }
+};
+menuToggle.addEventListener('click', function () {
+    if (navList.style.display === 'block') {
+        navList.style.display = 'none';
+    } else {
+        navList.style.display = 'block';
+    }
+});
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 1300) {
+        navList.style.display = 'flex';
+    } else {
+        navList.style.display = 'none';
+    }
+});
